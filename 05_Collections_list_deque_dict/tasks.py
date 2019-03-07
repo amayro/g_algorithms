ef5b67faff02b5e681ddc27a4a3d52549668a563
amayro <aabmstu@gmail.com>
2019-03-07 21:16:37 +0300

    add 05_tasks 2 var

--- 05_Collections_list_deque_dict/tasks.py
+++ 05_Collections_list_deque_dict/tasks.py
@@ -112,3 +112,23 @@ num_1 * num_2
 
 print(list(num_1.sum))
 print(list(num_1.mult))
+
+
+#  Вариант №2
+print()
+num_1 = 'A2'
+num_2 = 'C4F'
+
+num_1, num_2 = list(num_1), list(num_2)
+print(num_1)
+print(num_2)
+
+lst_convert_hex_int = [int(''.join(i), 16) for i in [num_1, num_2]]
+sum_ = sum(lst_convert_hex_int)
+mult = reduce(lambda x, y:  x * y, lst_convert_hex_int)
+
+hex_sum = list(hex(sum_).upper()[2:])
+hex_mult = list(hex(mult).upper()[2:])
+
+print(hex_sum)
+print(hex_mult)
